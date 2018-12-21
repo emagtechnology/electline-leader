6179d2e835f6826c179e1d19c5e469a8980bbac4
Ankit <ujlayanankit>
2018-12-21 14:14:43 +0530

    Create Project

---
+++ our-leader/src/main/java/com/leader/app/OurLeaderApplication.java
@@ -0,0 +1,14 @@
+package com.leader.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurLeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurLeaderApplication.class, args);
+	}
+
+}
+
